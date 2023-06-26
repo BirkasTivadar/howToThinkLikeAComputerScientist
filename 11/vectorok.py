3e12f8579e7dce64d4c5e42a5540bdb0e7b81473
BirkasTivadar <birkastivadar@gmail.com>
2023-06-26 21:46:15 +0200

    11. fejezet 6. feladat kész

--- 11/vectorok.py
+++ 11/vectorok.py
@@ -19,3 +19,20 @@ def vektorok_osszege(vector1: list, vector2: list):
 teszt(vektorok_osszege([1, 1], [1, 1]) == [2, 2])
 teszt(vektorok_osszege([1, 2], [1, 4]) == [2, 6])
 teszt(vektorok_osszege([1, 2, 1], [1, 4, 3]) == [2, 6, 4])
+
+
+"""
+6. Írj egy szorzas_skalarral(s, v) függvényt, amely paraméterként egy s számot, és egy v listát kap, és visszatér a függvény a v lista s skalárral való szorzatával.
+"""
+
+
+def szorzas_skalarral(szam: int, vector: list):
+    eredmeny: list = []
+    for elem in vector:
+        eredmeny.append(szam * elem)
+    return eredmeny
+
+
+teszt(szorzas_skalarral(5, [1, 2]) == [5, 10])
+teszt(szorzas_skalarral(3, [1, 0, -1]) == [3, 0, -3])
+teszt(szorzas_skalarral(7, [3, 0, 5, 11, 2]) == [21, 0, 35, 77, 14])
