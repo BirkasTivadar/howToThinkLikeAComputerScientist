b6606ebdec8a983fce33b3ffab406b316e04fe2b
BirkasTivadar <birkastivadar@gmail.com>
2023-07-01 10:32:00 +0200

    12. fejezet 3. feladat kész

--- 12/feladat3.py
+++ 12/feladat3.py
@@ -5,6 +5,6 @@ Mit csinál a deepcoopy?
 # A deepcopy az objektumok belső objektum argumentumait is másolja
 
 """
-A legutóbbi fejezet mely feladataiban lenne hasznos a deecoopy használata?
+A legutóbbi fejezet mely feladataiban lenne hasznos a deepcoopy használata?
 """
 # A beágyazott listáknál
