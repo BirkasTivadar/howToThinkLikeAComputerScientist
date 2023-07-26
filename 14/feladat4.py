f44e801b42081470421b7c4388287b4265fcd645
BirkasTivadar <birkastivadar@gmail.com>
2023-07-26 22:10:51 +0200

    14. fejezet 4. feladat a. részfeladat kész

--- 14/feladat4.py
+++ 14/feladat4.py
@@ -25,9 +25,3 @@ teszt(yTukrozes([0, 4, 7, 5, 2, 6, 1, 3]) == [3, 1, 6, 2, 5, 7, 4, 0])
 teszt(not yTukrozes([0, 4, 7, 5, 2, 6, 1, 3]) == [0, 6, 4, 7, 1, 3, 5, 2])
 teszt(yTukrozes([7, 3, 0, 2, 5, 1, 6, 4]) == [4, 6, 1, 5, 2, 0, 3, 7])
 
-
-"""
-b. Írj egy függvényt, amely egy megoldást tükröz az X tengelyre.
-"""
-
-def
