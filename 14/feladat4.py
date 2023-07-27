2b44a8ecf14edd971ca92743f8b1901e7471aa65
BirkasTivadar <birkastivadar@gmail.com>
2023-07-27 23:00:44 +0200

    14. fejezet 4. feladat d. részfeladat félig kész

--- 14/feladat4.py
+++ 14/feladat4.py
@@ -15,7 +15,7 @@ from egyseg_teszt import *
 
 
 def yTukrozes(alapFelallas: list):
-    tukrozott = []
+    tukrozott: list = []
     for i in range(-1, -len(alapFelallas) - 1, -1):
         tukrozott.append(alapFelallas[i])
     return tukrozott
@@ -31,7 +31,7 @@ b. Írj egy függvényt, amely egy megoldást tükröz az X tengelyre.
 
 
 def xTukrozes(alapFelallas: list):
-    tukrozott = []
+    tukrozott: list = []
     for number in alapFelallas:
         tukrozott.append(7 - number)
     return tukrozott
@@ -41,3 +41,29 @@ teszt(xTukrozes([0, 4, 7, 5, 2, 6, 1, 3]) == [7, 3, 0, 2, 5, 1, 6, 4])
 teszt(not xTukrozes([0, 4, 7, 5, 2, 6, 1, 3]) == [2, 5, 3, 1, 7, 4, 6, 0])
 teszt(xTukrozes([5, 2, 4, 6, 0, 3, 1, 7]) == [2, 5, 3, 1, 7, 4, 6, 0])
 
+"""
+c. Írj egy függvényt, amely a megoldást elforgatja 90 fokkal az óra járásának ellentétesen, 
+és használja a 180 és 270 fokos forgatásokat is.
+"""
+
+
+# def elforgatasBalra(alapFelallas: list):
+#     tukrozott: list = [0, 0, 0, 0, 0, 0, 0, 0]
+#     for number in alapFelallas:
+#         tukrozott[number] = alapFelallas.index(7 - number)
+#     return tukrozott
+#
+#
+# teszt(elforgatasBalra([0, 4, 7, 5, 2, 6, 1, 3]) == [7, 1, 3, 0, 6, 4, 2, 5])
+# teszt(not elforgatasBalra([0, 4, 7, 5, 2, 6, 1, 3]) == [2, 5, 3, 1, 7, 4, 6, 0])
+
+
+def elforgatasJobbra(alapFelallas: list):
+    tukrozott: list = [0, 0, 0, 0, 0, 0, 0, 0]
+    for number in alapFelallas:
+        tukrozott[7 - number] = alapFelallas.index(number)
+    return tukrozott
+
+
+teszt(elforgatasJobbra([0, 4, 7, 5, 2, 6, 1, 3]) == [2, 5, 3, 1, 7, 4, 6, 0])
+teszt(not elforgatasJobbra([0, 4, 7, 5, 2, 6, 1, 3]) == [7, 1, 3, 0, 6, 4, 2, 5])
